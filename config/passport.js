1e0892037fe788045247d79c2445dd3928d7bd89
Diogo Righi <diogorighi@gmail.com>
2016-06-01 14:12:03 -0300

    change callback url to github login

--- config/passport.js
+++ config/passport.js
@@ -57,7 +57,7 @@ passport.use('local-login',new LocalStrategy(
 passport.use('github', new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/github/callback"
+    callbackURL: "https://nameless-shelf-74727.herokuapp.com/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function(){
